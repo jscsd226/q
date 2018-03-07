3e373a559bb86ae6d6f6e41d21fa1d8435a0a5c3
jscsd226 <305591377@qq.com>
2018-03-07 09:45:49 +0800

    first commit

---
+++ new.java
@@ -0,0 +1,20 @@
+class Person{
+	
+	String name;
+	int age;
+	
+}
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	Person p = new Person();
+	p.name="狗娃";
+	p.age = 18;		
+    System.out.println(p.name+"今年"+p.age+"岁");
+	}
+
+}
