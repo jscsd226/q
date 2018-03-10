917e2a6f910def2b5e4a9b0067827d3203bfd711
jscsd226 <305591377@qq.com>
2018-03-10 10:06:38 +0800

    实验性的文件、

---
+++ q.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		Object o = new Object();
+		
+		
+		System.out.println(o);
+		System.out.println("toString "+o.toString());
+
+	}
+}
